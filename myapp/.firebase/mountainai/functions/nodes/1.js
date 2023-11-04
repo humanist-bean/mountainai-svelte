2d2e7e466ab1ff317e17181c2f4d9799cdb28182
Alder French <alderfrench1@gmail.com>
2023-11-03 18:31:15 -0700

    It looks like google app engine mae some changes that broke my backend a week or two ago, so I reconfigured it by updating the yaml file to use a python runtime instead of a custom runtime so it will work with their new setup. Note that the Dockerfile is no longer being used with this current setup. Oh and it should cost considerably less to run the website now because it doesn't have a google app engine instance running 24/7 anymore.

--- myapp/.firebase/mountainai/functions/nodes/1.js
+++ myapp/.firebase/mountainai/functions/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.aecd3705.js","_app/immutable/chunks/scheduler.9341ea61.js","_app/immutable/chunks/index.66f07722.js","_app/immutable/chunks/stores.a2cb788f.js","_app/immutable/chunks/singletons.5465c374.js","_app/immutable/chunks/index.9eb50c3c.js"];
+export const imports = ["_app/immutable/nodes/1.effd417b.js","_app/immutable/chunks/scheduler.9341ea61.js","_app/immutable/chunks/index.66f07722.js","_app/immutable/chunks/stores.5ff95eee.js","_app/immutable/chunks/singletons.1f96287d.js","_app/immutable/chunks/index.9eb50c3c.js"];
 export const stylesheets = [];
 export const fonts = [];
